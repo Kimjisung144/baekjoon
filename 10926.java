a1e078f8254768002e7ba49b6b09c79faf722991
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-03-04 17:47:18 +0900

    Create 10926.java

---
+++ 10926.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String ch;
+        Scanner sc = new Scanner(System.in);
+        ch = sc.next();
+        System.out.println(ch+"??!");
+    }
+}
