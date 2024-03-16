b1c21022e5f85aaa296298b4c13502e18a501b9b
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-03-16 17:28:20 +0900

    Create 2438.java

---
+++ 2438.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+       for(int i=5;i>=1; i--)
+       {
+           for(int j=0; j<=(5-i);j++)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+    }
+}
