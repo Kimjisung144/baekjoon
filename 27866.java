45f54efa639452682b47588924cdf370f727ff9e
Kimjisung144 <48574631+Kimjisung144@users.noreply.github.com>
2024-06-22 05:40:09 +0900

    Create 27866.java

---
+++ 27866.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+        int n;
+        String str = new String();
+        n = sc.nextInt()-1;
+       System.out.println(str.substring(n,n+1));
+       }
+}
